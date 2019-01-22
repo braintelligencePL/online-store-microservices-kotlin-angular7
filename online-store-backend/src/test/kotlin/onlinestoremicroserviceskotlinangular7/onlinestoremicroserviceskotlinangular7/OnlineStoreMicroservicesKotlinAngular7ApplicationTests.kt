cc841d72f6e40d91f0b8fa18e943afb40346cb11
lsroczynski <lsroczynski@consdata.pl>
2019-01-22 09:19:44 +0100

    init

---
+++ online-store-backend/src/test/kotlin/onlinestoremicroserviceskotlinangular7/onlinestoremicroserviceskotlinangular7/OnlineStoreMicroservicesKotlinAngular7ApplicationTests.kt
@@ -0,0 +1,17 @@
+package onlinestoremicroserviceskotlinangular7.onlinestoremicroserviceskotlinangular7
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class OnlineStoreMicroservicesKotlinAngular7ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
