4074885ce5e3b002f519d0f6bcb0140c222167d5
braintelligencePL <lsroczynski@consdata.pl>
2019-02-06 16:02:57 +0100

    dockerfile changes - jdk-12 support

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ set -e # if something breaks stop the script (not always good thing)
 ## infrastructure stuff ##
 cd infrastructure/spring-cloud && ./gradlew clean build buildDocker && cd ../..
 
-
 ## application backend ##
 cd online-store-backend && ./gradlew clean build buildDocker && cd ..
 
