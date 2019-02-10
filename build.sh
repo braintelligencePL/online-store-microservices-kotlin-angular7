20e7f583ea034139f49da64e78c5a30b3c0f04f3
braintelligencePL <iluzion5@o2.pl>
2019-02-10 21:34:52 +0100

    remove trash

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 set -e # if something breaks stop the script (not always good thing)
 
-## infrastructure stuff ##
-cd infrastructure/spring-cloud && ./gradlew clean build buildDocker && cd ../..
+## infrastructure spring-cloud & monitoring 
+cd online-store-infrastructure/spring-cloud && ./gradlew clean build buildDocker && cd ../..
 
-## application backend ##
+## application backend 
 cd online-store-backend && ./gradlew clean build buildDocker && cd ..
 
 ## start all images ##
